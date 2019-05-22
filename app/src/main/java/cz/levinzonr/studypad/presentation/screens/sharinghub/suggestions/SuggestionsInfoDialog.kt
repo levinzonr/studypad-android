fa5f1a947f71373b875ac406ca8a67a5bd4a5ced
Roman Levinzon <levinzon.roman@gmail.com>
2019-05-22 17:30:27 +0200

    Preview suggestions in suggestions list

--- app/src/main/java/cz/levinzonr/studypad/presentation/screens/sharinghub/suggestions/SuggestionsInfoDialog.kt
+++ app/src/main/java/cz/levinzonr/studypad/presentation/screens/sharinghub/suggestions/SuggestionsInfoDialog.kt
@@ -12,31 +12,81 @@ import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import cz.levinzonr.studypad.R
+import cz.levinzonr.studypad.domain.models.PublishedNotebook
+import cz.levinzonr.studypad.domain.models.modType
+import cz.levinzonr.studypad.layoutInflater
+import cz.levinzonr.studypad.setVisible
+import kotlinx.android.synthetic.main.include_note.view.*
+import kotlinx.android.synthetic.main.item_suggestion_updated.view.*
+import timber.log.Timber
 
 class SuggestionsInfoDialog : DialogFragment() {
 
+    private val suggestion: SuggestionsModels.SuggestionItem?
+        get() = arguments?.getParcelable(TAG)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-    }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+   /* override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog =  AlertDialog.Builder(context)
-            .setView(R.layout.dialog_suggesions_info)
+            .setView(buildView(LayoutInflater.from(context)))
             .create()
-        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
         val dialogLayoutParams = dialog?.window?.attributes
         dialog.window?.attributes = dialogLayoutParams
         return dialog
     }
+*/
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return  buildView(inflater)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+        val dialogLayoutParams = dialog?.window?.attributes
+        dialog?.window?.attributes = dialogLayoutParams
+    }
+
+
+    private fun buildView(layoutInflater: LayoutInflater) : View {
+        return if (suggestion?.suggestion?.type?.modType == PublishedNotebook.ModificationType.Updated) {
+             layoutInflater.inflate(R.layout.item_suggestion_updated, null, false)
+                 .bindUpdatedView(suggestion)
+        } else {
+             layoutInflater.inflate(R.layout.item_suggestion_added, null, false)
+                 .bindAddedView(suggestion)
+        }
+    }
+
+    private fun View.bindAddedView(suggestion: SuggestionsModels.SuggestionItem?) : View {
+        val suggestionItem = suggestion ?: return this
+        val author = context.getString(R.string.suggestion_author, suggestionItem.suggestion.author.displayName)
+        suggestionTitle.text = context.getString(R.string.suggestion_type_add, author)
+        noteTitleTv.text = suggestionItem.suggestion.title
+        Timber.d("sugg: ${suggestion.suggestion.content}")
+        noteContentTv.text = suggestionItem.suggestion.content
+        approveBtn.setVisible(false, View.INVISIBLE)
+        rejectedBtn.setVisible(false, View.INVISIBLE)
+        return this
+    }
+
+    private fun View.bindUpdatedView(suggestion: SuggestionsModels.SuggestionItem?) : View {
+        val suggestionItem = suggestion ?: return this
+        val author = context.getString(R.string.suggestion_author, suggestionItem.suggestion.author.displayName)
+        noteTitleTv.text = suggestionItem.suggestion.title
+        noteContentTv.text = suggestionItem.suggestion.content
+        noteOldTitleTv.text = suggestionItem.sourceNote?.title
+        noteOldContentTv.text = suggestionItem.sourceNote?.content
+        suggestionTitle.text = context.getString(R.string.suggestion_type_upd, author)
+        approveBtn.setVisible(false, View.INVISIBLE)
+        rejectedBtn.setVisible(false, View.INVISIBLE)
+        return this
+    }
 
     companion object {
         private const val TAG = "tag:suggest"
-        fun show(fm: FragmentManager) {
+        fun show(fm: FragmentManager, suggestionItem: SuggestionsModels.SuggestionItem) {
             val dialog = fm.findFragmentByTag(TAG) as? SuggestionsInfoDialog? ?: SuggestionsInfoDialog()
+            dialog.arguments = Bundle().apply { putParcelable(TAG, suggestionItem) }
             dialog.show(fm, TAG)
         }
     }
